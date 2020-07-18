8021092570b33a5cc22b04fdc32e4386a2382dc0
JosemaPereira <josema.pereira.cih@gmail.com>
2020-07-18 01:49:35 -0500

    Base project

---
+++ app/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { injectIntl } from 'react-intl';
+import { mainMessages } from './messages';
+
+export const Main = injectIntl(({ intl }) => (
+  <header className="container-fluid">
+    <div className="row">
+      <div className="col">
+        <h1>{intl.formatMessage(mainMessages.title)}</h1>
+      </div>
+    </div>
+  </header>
+));
